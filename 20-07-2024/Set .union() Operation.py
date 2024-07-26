ac5d37b76e8df369a927878cb2ede01c2c034018
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2024-07-26 19:28:25 +0530

    Delete 20-07-2024/Set .union() Operation.py

--- 20-07-2024/Set .union() Operation.py
+++
@@ -1,9 +0,0 @@
-
-n = int(input())
-
-english_subscribers = set(map(int, input().split()))
-
-m = int(input())
-french_subscribers = set(map(int, input().split()))
-all_subscribers = english_subscribers.union(french_subscribers)
-print(len(all_subscribers))
