20fdcc4e6cdbf5fb5e6a89db6b751067d3067b3a
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2024-07-26 19:28:10 +0530

    Delete 20-07-2024/Set .intersection() Operation.py

--- 20-07-2024/Set .intersection() Operation.py
+++
@@ -1,8 +0,0 @@
-
-n = int(input())
-english_subscribers = set(map(int, input().split()))
-m = int(input())
-french_subscribers = set(map(int, input().split()))
-both_subscribers = english_subscribers.intersection(french_subscribers)
-
-print(len(both_subscribers))
